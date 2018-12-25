c65f24a8a3d5943662ad85af6b51c9848f1b22c4
Marko Atanasievski <markoatana@gmail.com>
2018-12-25 13:54:48 +0100

    Setting project basic layout

---
+++ AUTHORS.md
@@ -0,0 +1,10 @@
+# Credits
+
+## Development Lead
+
+- Pavle Batuta [paxthemax](https://github.com/paxthemax)
+- Marko Atanasievski [atanmarko](https://github.com/atanmarko)
+
+## Contributors
+
+None yet. Why not be the first?
---
+++ cmd/safexdrpc_client.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/atanmarko/gosafex/pkg/safexdrpc"
+	"github.com/spf13/cobra"
+)
+
+var daemonHost string
+var daemonPort int
+
+// safexdRpcCmd represents the RPC daemon api test command
+var safexdRpcCmd = &cobra.Command{
+	Use:   "safexdrpc",
+	Short: "Test cmd rpc client for safex daemon",
+	Long:  `Cmd that talks to safexd rpc daemon and prints basic info about daemon`,
+	Args:  cobra.MinimumNArgs(0),
+	Run: func(cmd *cobra.Command, args []string) {
+
+		fmt.Println("Connecting to host ", daemonHost, " port ", daemonPort)
+
+		fmt.Println("Safex Node Info:", safexdrpc.GetInfo)
+
+	},
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringVarP(&daemonHost, "daemon_host", "d", "", "Target safex daemon host")
+	rootCmd.PersistentFlags().IntVar(&daemonPort, "daemon_port", 29393, "Target safex daemon port")
+	rootCmd.AddCommand(safexdRpcCmd)
+}
