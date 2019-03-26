bc12ad4b4018776eef484eca9b5f95b542c3d6d5
Pavle Batuta <pavle.batuta@protonmail.com>
2019-03-26 14:35:05 +0100

    Quick key derivation implementation
    Implementation of key derivation based on a third-party curve library.
    
    TODO: refactor or include a different third-party package.

---
+++ internal/crypto/derivation/util.go
@@ -0,0 +1,12 @@
+package derivation
+
+import "encoding/hex"
+
+func HexToKey(h string) (result Key) {
+	byteSlice, _ := hex.DecodeString(h)
+	if len(byteSlice) != 32 {
+		panic("Incorrect key size")
+	}
+	copy(result[:], byteSlice)
+	return
+}
