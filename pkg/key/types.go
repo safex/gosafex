a6cdb69e15352555eafe89375e0165bb9e506870
Pavle Batuta <pavle.batuta@protonmail.com>
2019-04-10 09:25:19 +0200

    Removed unused types

--- pkg/key/types.go
+++ pkg/key/types.go
@@ -2,30 +2,11 @@ package key
 
 import (
 	"github.com/safex/gosafex/internal/crypto"
+	"github.com/safex/gosafex/pkg/common"
 )
 
 // Digest is an alias to crypto.Digest.
 type Digest = crypto.Digest
 
-// ToByteSerializer can serialize itself as []byte.
-type ToByteSerializer interface {
-	ToBytes() []byte
-}
-
-// KeySize is the size of the key seed (in bytes).
-const KeySize = crypto.KeySize
-
-// PrivateKey is an elliptic curve point.
-type PrivateKey crypto.PrivateKey
-
-// PublicKey is the public keypair of PrivateKey.
-type PublicKey crypto.PublicKey
-
-// SeedLength is the size of the key seed (in bytes).
-const SeedLength = KeySize
-
-// Seed is a (usually random) seqence of bytes.
-type Seed []byte
-
-// CurveKey is the alias to crypto.CurveKey
-type CurveKey = crypto.CurveKey
+// ToByteSerializer is an alias to common.ToByteSerializer.
+type ToByteSerializer = common.ToByteSerializer
