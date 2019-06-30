b0efe1a4d84edd7c391462f7c7156d4fc10ff6a6
Stefan Isidorovic <isidorovicstefan@gmail.com>
2019-06-30 19:36:09 +0200

    Added ground for Wallet JSON RPC.

---
+++ pkg/rpc/errors.go
@@ -0,0 +1,20 @@
+package SafexRPC
+
+import (
+
+)
+
+type StatusCodeError uint16
+
+// List of error status codes
+const (
+	EverythingOK = 0
+	ReadingRequestError = 1
+	JSONRqMalformed = 2
+	JSONRsMalformed = 3
+	
+
+
+
+)
+
