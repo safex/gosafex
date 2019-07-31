3bf05020074142bfeec6a26c7db19b8e13094593
edo3 <et1@tiscali.it>
2019-07-31 14:35:53 +0200

    Corrected routes name

--- pkg/rpc/routes.go
+++ pkg/rpc/routes.go
@@ -19,19 +19,19 @@ func (w *WalletRPC) GetRoutes() (routes []Route) {
 	routes = append(routes, Route{"RecoverWithKeys", "POST", "/init/recover-keys", w.RecoverWithKeys})
 	routes = append(routes, Route{"Status", "POST", "/status", w.GetStatus})
 	routes = append(routes, Route{"GetAccountInfo", "POST", "/account/info", w.GetAccountInfo})
-	routes = append(routes, Route{"GetAccountInfo", "POST", "/account/open", w.OpenAccount})
+	routes = append(routes, Route{"OpenAccount", "POST", "/account/open", w.OpenAccount})
 
 	routes = append(routes, Route{"StoreData", "POST", "/store/put", w.StoreData})
 	routes = append(routes, Route{"LoadData", "POST", "/store/get", w.LoadData})
 
-	routes = append(routes, Route{"GetAccountInfo", "GET", "/transaction/get", w.GetTransactionInfo})
-	routes = append(routes, Route{"GetAccountInfo", "GET", "/transaction/history", w.GetHistory})
-	routes = append(routes, Route{"GetAccountInfo", "GET", "/transaction/history-up-to", w.GetTransactionUpToBlockHeight})
+	routes = append(routes, Route{"GetTransactionInfo", "GET", "/transaction/get", w.GetTransactionInfo})
+	routes = append(routes, Route{"GetHistory", "GET", "/transaction/history", w.GetHistory})
+	routes = append(routes, Route{"GetTransactionUpToBlockHeight", "GET", "/transaction/history-up-to", w.GetTransactionUpToBlockHeight})
 
-	routes = append(routes, Route{"GetAccountInfo", "GET", "/output/get", w.GetOutputInfo})
-	routes = append(routes, Route{"GetAccountInfo", "GET", "/output/get-from-tx", w.GetOutputInfoFromTransaction})
-	routes = append(routes, Route{"GetAccountInfo", "GET", "/output/get-from-type", w.GetOutputInfoFromType})
-	routes = append(routes, Route{"GetAccountInfo", "GET", "/output/get-unspent", w.GetUnspentOutputs})
+	routes = append(routes, Route{"GetOutputInfo", "GET", "/output/get", w.GetOutputInfo})
+	routes = append(routes, Route{"GetOutputInfoFromTransaction", "GET", "/output/get-from-tx", w.GetOutputInfoFromTransaction})
+	routes = append(routes, Route{"GetOutputInfoFromType", "GET", "/output/get-from-type", w.GetOutputInfoFromType})
+	routes = append(routes, Route{"GetUnspentOutputs", "GET", "/output/get-unspent", w.GetUnspentOutputs})
 
 	return routes
 }
