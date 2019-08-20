a1eb78e3ec0c094e681babe531f54a8c1451ce63
Stefan Isidorović <isidorovicstefan@gmail.com>
2019-08-20 09:27:03 +0200

    Added functions for serializing formed tx to hex

--- pkg/chain/balance/commit_ptx.go
+++ pkg/chain/balance/commit_ptx.go
@@ -1,7 +1,9 @@
 package balance
 
 import (
-	"github.com/golang/glog"
+	"encoding/hex"
+
+	"github.com/golang/protobuf/proto"
 	"github.com/safex/gosafex/pkg/safex"
 )
 
@@ -10,10 +12,44 @@ import (
 // Commiting pending transaction to node for insertion in
 // blockchain.
 func (w *Wallet) CommitPtx(ptx *PendingTx) (res safex.SendTxRes, err error) {
-	glog.Info("CommitTx: Commiting transaction: ", *ptx.Tx)
-	// fmt.Println("Inputs!")
-	// for _, input := range ptx.Tx.Vin {
-	// 	fmt.Println(input)
-	// }
-	return w.client.SendTransaction(ptx.Tx, false)
+	// glog.Info("CommitTx: Commiting transaction: ", *ptx.Tx)
+	// // fmt.Println("Inputs!")
+	// // for _, input := range ptx.Tx.Vin {
+	// // 	fmt.Println(input)
+	// // }
+	// return w.client.SendTransaction(ptx.Tx, false)
+
+	input, err := w.GetHexStringFromPtx(ptx)
+	if err != nil {
+		return res, err
+	}
+
+	res, err = w.SendTxFromString(input)
+	if err != nil {
+		return res, err
+	}
+	return
+}
+
+func (w *Wallet) GetHexStringFromPtx(ptx *PendingTx) (string, error) {
+	bytesTx, err := proto.Marshal(ptx.Tx)
+	if err != nil {
+		return "", err
+	}
+	ret := hex.EncodeToString(bytesTx)
+	return ret, nil
+}
+
+func (w *Wallet) SendTxFromString(input string) (res safex.SendTxRes, err error) {
+	var tx safex.Transaction
+	buf, err := hex.DecodeString(input)
+	if err != nil {
+		return res, err
+	}
+	err = proto.Unmarshal(buf, &tx)
+	if err != nil {
+		return res, err
+	}
+
+	return w.client.SendTransaction(&tx, false)
 }
